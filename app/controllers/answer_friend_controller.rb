8920efc93a79c7ccd907e26ec81d12030d96c572
Sungu Nam <naisz76@gmail.com>
2016-04-06 08:41:59 +0000

    real error change

--- app/controllers/answer_friend_controller.rb
+++ app/controllers/answer_friend_controller.rb
@@ -3,12 +3,14 @@ class AnswerFriendController < ApplicationController
     end
     
     def friend_res
-      @friend_id = params[:friend_id]
+      @friend_id = params[:friend_id].to_i
       @story_list = Story.all
     end
     
     def update_by_friend
       Response.create(user_id: current_user.id, question_id: params[:question_id].to_i, my_ans: params[:my_ans].to_i, target_id: params[:target_id].to_i)
-      redirect_to :back, friend_id: params[:target_id].to_i, notice: "질문에 답하셨습니다."
+      flash[:notice] = "질문에 답하셨습니다."
+      redirect_to :controller => 'answer_friend', :action => 'friend_res', :friend_id => params[:target_id]
+      #redirect_to :back, friend_id: (params[:target_id].to_i), notice: "질문에 답하셨습니다."
     end
 end
