fe69f3b78c6d66032a4532e6fd4015606911f520
Sungu Nam <naisz76@gmail.com>
2016-04-06 07:52:47 +0000

    error solve!

--- app/controllers/answer_friend_controller.rb
+++ app/controllers/answer_friend_controller.rb
@@ -8,7 +8,7 @@ class AnswerFriendController < ApplicationController
     end
     
     def update_by_friend
-      Response.create(user_id: current_user.id, question_id: params[:question_id].to_i, my_ans: params[:my_ans].to_i, target_id: User.where(uid: params[:target_id]).ids[0])
-      redirect_to :back, friend_id: params[:target_id], notice: "질문에 답하셨습니다."
+      Response.create(user_id: current_user.id, question_id: params[:question_id].to_i, my_ans: params[:my_ans].to_i, target_id: params[:target_id].to_i)
+      redirect_to :back, friend_id: params[:target_id].to_i, notice: "질문에 답하셨습니다."
     end
 end
